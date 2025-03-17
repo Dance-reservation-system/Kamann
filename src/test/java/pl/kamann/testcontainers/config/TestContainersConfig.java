913ec0f07dba5ed688d9a447289143de332e5d66
F1iper <ftimofiejew@gmail.com>
2025-03-17 22:23:24 +0100

    Refactor: Testcontainers reworked

--- src/test/java/pl/kamann/testcontainers/config/TestContainersConfig.java
+++ src/test/java/pl/kamann/testcontainers/config/TestContainersConfig.java
@@ -9,15 +9,20 @@ import org.testcontainers.utility.DockerImageName;
 
 @TestConfiguration
 public class TestContainersConfig {
-
     private static final PostgreSQLContainer<?> POSTGRES_CONTAINER;
 
     static {
+        System.out.println("Initializing PostgreSQL container for tests...");
         POSTGRES_CONTAINER = new PostgreSQLContainer<>(DockerImageName.parse("postgres:16"))
                 .withDatabaseName("testdb")
                 .withUsername("test")
                 .withPassword("test");
+
+        // Start container
         POSTGRES_CONTAINER.start();
+
+        // Log the JDBC URL for debugging
+        System.out.println("PostgreSQL container started with URL: " + POSTGRES_CONTAINER.getJdbcUrl());
     }
 
     @DynamicPropertySource
@@ -25,10 +30,5 @@ public class TestContainersConfig {
         registry.add("spring.datasource.url", POSTGRES_CONTAINER::getJdbcUrl);
         registry.add("spring.datasource.username", POSTGRES_CONTAINER::getUsername);
         registry.add("spring.datasource.password", POSTGRES_CONTAINER::getPassword);
-    }
-
-    @Bean
-    public PostgreSQLContainer<?> postgreSQLContainer() {
-        return POSTGRES_CONTAINER;
     }
 }
