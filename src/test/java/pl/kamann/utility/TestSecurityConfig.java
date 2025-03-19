7adf9e964d184d401187b688d3a4d3939645e021
F1iper <ftimofiejew@gmail.com>
2025-03-19 16:06:30 +0100

    Refactor: Introduce H2 db for workflow

---
+++ src/test/java/pl/kamann/utility/TestSecurityConfig.java
@@ -0,0 +1,26 @@
+package pl.kamann.utility;
+
+import io.jsonwebtoken.security.Keys;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
+import pl.kamann.config.security.jwt.JwtUtils;
+
+import javax.crypto.SecretKey;
+import java.util.Arrays;
+
+@Configuration
+@Profile("test")
+public class TestSecurityConfig {
+    
+    @Bean
+    @Primary
+    public JwtUtils jwtUtils() {
+        byte[] keyBytes = new byte[32];
+        Arrays.fill(keyBytes, (byte) 1);
+        SecretKey testKey = Keys.hmacShaKeyFor(keyBytes);
+        
+        return new JwtUtils(testKey, 3600000L); // 1 hour expiration
+    }
+}
