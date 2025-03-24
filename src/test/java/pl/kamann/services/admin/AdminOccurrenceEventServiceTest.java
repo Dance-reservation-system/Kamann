5c64e7e8c87a7d659aa188db3c54a1e5d235508e
Moher <47338928+newmon13@users.noreply.github.com>
2025-03-24 02:23:50 +0100

    Added missing imports

--- src/test/java/pl/kamann/services/admin/AdminOccurrenceEventServiceTest.java
+++ src/test/java/pl/kamann/services/admin/AdminOccurrenceEventServiceTest.java
@@ -24,12 +24,13 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
+import static java.util.Collections.emptyList;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.doNothing;
-import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
