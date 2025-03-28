a6dd2e8b9c7d742a5548cdb5bb4d18fe981d47fe
fox-programmer <foxyfnaf.93yt@gmail.com>
2025-03-28 20:21:29 +0100

    create getUserDetails endpoint

--- src/main/java/pl/kamann/controllers/client/ClientAccountControler.java
+++ src/main/java/pl/kamann/controllers/client/ClientAccountControler.java
@@ -2,9 +2,7 @@ package pl.kamann.controllers.client;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import pl.kamann.dtos.UserDetailsDto;
 import pl.kamann.services.client.ClientAccountService;
 
@@ -13,10 +11,17 @@ import pl.kamann.services.client.ClientAccountService;
 @RequiredArgsConstructor
 public class ClientAccountControler {
 
-    ClientAccountService clientAccountService;
+    private final ClientAccountService clientAccountService;
 
     @GetMapping
     public ResponseEntity<UserDetailsDto> getUserDetails() {
         return ResponseEntity.ok(clientAccountService.getUserDetails());
+    }
+
+    @PutMapping
+    public ResponseEntity<UserDetailsDto> updateUserDetails(
+            @RequestBody() UserDetailsDto userDetailsDto
+    ) {
+        return ResponseEntity.ok(clientAccountService.updateUserDetails(userDetailsDto));
     }
 }
