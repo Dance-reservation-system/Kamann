61b13e121f409eb9fb71b9e037996773e5737e12
fox-programmer <foxyfnaf.93yt@gmail.com>
2025-03-28 14:30:29 +0100

    create getUserDetails endpoint

---
+++ src/main/java/pl/kamann/mappers/UserDetailsMapper.java
@@ -0,0 +1,16 @@
+package pl.kamann.mappers;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import pl.kamann.dtos.UserDetailsDto;
+import pl.kamann.entities.appuser.AppUser;
+import pl.kamann.entities.appuser.AuthUser;
+
+@Mapper(componentModel = "spring")
+public interface UserDetailsMapper {
+    @Mapping(target = "email", source = "authUser.email")
+    @Mapping(target = "phone", source = "appUser.phone")
+    @Mapping(target = "firstName", source = "appUser.firstName")
+    @Mapping(target = "lastName", source = "appUser.lastName")
+    UserDetailsDto toUserDetailsDto(AuthUser authUser, AppUser appUser);
+}
