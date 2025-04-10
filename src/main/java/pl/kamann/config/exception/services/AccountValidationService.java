818d220dc9523ffd23b2da35d3d8c6cc188be276
Fox Programmer <foxyfnaf.93yt@gmail.com>
2025-04-10 20:29:31 +0200

    Kmn 78 common edit personal details (#52)
    Co-authored-by: F1iper

---
+++ src/main/java/pl/kamann/config/exception/services/AccountValidationService.java
@@ -0,0 +1,80 @@
+package pl.kamann.config.exception.services;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import pl.kamann.config.codes.StatusCodes;
+import pl.kamann.config.exception.handler.ApiException;
+import pl.kamann.dtos.UserDetailsDto;
+
+@RequiredArgsConstructor
+@Service
+public class AccountValidationService {
+
+    private final ValidationService validationService;
+
+    public void validateUpdateRequest(UserDetailsDto requestDto) {
+        if (requestDto == null) {
+            throw new ApiException(
+                    "Request cannot be null",
+                    HttpStatus.BAD_REQUEST,
+                    StatusCodes.INVALID_INPUT.name()
+            );
+        }
+
+        if (requestDto.firstName() != null) {
+            validateUserFirstName(requestDto.firstName());
+        }
+        if (requestDto.lastName() != null) {
+            validateUserLastName(requestDto.lastName());
+        }
+        if (requestDto.phone() != null) {
+            validatePhone(requestDto.phone());
+        }
+        if (requestDto.email() != null) {
+            validateEmail(requestDto.email());
+        }
+    }
+
+    private void validateEmail(String email) {
+        if (!isValidEmailFormat(email)) {
+            throw new ApiException("Invalid email format",
+                    HttpStatus.BAD_REQUEST,
+                    StatusCodes.INVALID_INPUT.name());
+        }
+
+        validationService.validateEmailNotTaken(email);
+    }
+
+    private boolean isValidEmailFormat(String email) {
+        return email != null && email.matches("^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$");
+    }
+
+    public void validateUserFirstName(String userFirstName) {
+        if (userFirstName.length() > 30 || userFirstName.length() < 2) {
+            throw new ApiException(
+                    "User First Name must have more than 2 and less than 30 letters",
+                    HttpStatus.BAD_REQUEST,
+                    StatusCodes.INVALID_INPUT.name()
+            );
+        }
+    }
+
+    public void validateUserLastName(String userLastName) {
+        if (userLastName.length() > 30 || userLastName.length() < 2) {
+            throw new ApiException(
+                    "User Last Name must have more than 2 and less than 30 letters",
+                    HttpStatus.BAD_REQUEST,
+                    StatusCodes.INVALID_INPUT.name()
+            );
+        }
+    }
+
+    public void validatePhone(String phone) {
+        if (phone == null || phone.length() != 9 || !phone.matches("\\d+")) {
+            throw new ApiException("Phone number must be exactly 9 digits",
+                    HttpStatus.BAD_REQUEST,
+                    StatusCodes.INVALID_INPUT.name());
+        }
+    }
+}
